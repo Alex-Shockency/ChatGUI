024163ab6b733929e6103d55bccb1644a9b46aee
Alexander Shockency <ashocken@indianapolis.cs.colostate.edu>
2015-10-16 16:35:25 -0600

    fixing master

--- simplechat1/ServerConsole.java
+++ simplechat1/ServerConsole.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import client.ChatClient;
-
 import common.ChatIF;
 
 public class ServerConsole implements ChatIF {
