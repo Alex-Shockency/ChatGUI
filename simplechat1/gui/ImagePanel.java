f97f5ac8f42c49bb3e56e38f02ccdeeee70a727f
Joshua Fuller <joshuaandrewfuller@gmail.com>
2015-11-15 23:41:48 -0700

    Added sketch and image GUI elements
    Added a JPanel element that will be able to create images
    Also, created a quick element to display images

---
+++ simplechat1/gui/ImagePanel.java
@@ -0,0 +1,21 @@
+package gui;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel{
+
+  private BufferedImage b;
+  
+  public ImagePanel(BufferedImage b){
+    this.b = b;
+  }
+  
+  @Override
+  protected void paintComponent(Graphics g){
+    super.paintComponent(g);
+    g.drawImage(b, 0, 0, null);
+  }
+  
+}
---
+++ simplechat1/gui/PaintPanel.java
@@ -0,0 +1,135 @@
+package gui;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class PaintPanel extends JPanel {
+  private static final long serialVersionUID = 4856815551206358400L;
+
+  public Image getImage(){
+    return canvas;
+  }
+  
+  private class drawAdapter extends MouseAdapter{
+    @Override
+    public void mouseDragged(MouseEvent e){
+      paint(e);
+    }
+    
+    @Override
+    public void mousePressed(MouseEvent e){
+      paint(e);
+    }
+    
+    @Override
+    public void mouseExited(MouseEvent e){
+      repaint();
+    }
+    
+    public void mouseMoved(MouseEvent e){
+      System.out.println("Moved");
+      repaint();
+    }
+    
+    public void paint(MouseEvent e){
+      Graphics2D canvasGraphics = canvas.createGraphics();
+      canvasGraphics.setColor(drawColor);
+      int x = e.getX() - (radius/2);
+      int y = e.getY() - (radius/2);
+      canvasGraphics.fillOval(x, y, radius, radius);
+      repaint();
+    }
+  }
+  
+  private BufferedImage canvas;
+  private Color drawColor;
+  public int radius = 10;
+  
+  public void initialize(){
+    drawColor = Color.black;
+    int width = (int) getSize().getWidth();
+    int height = (int) getSize().getHeight();
+    canvas = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+    Graphics2D canvasGraphics = canvas.createGraphics();
+    canvasGraphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    drawAdapter pencil = new drawAdapter();
+    this.addMouseListener(pencil);
+    this.addMouseMotionListener(pencil);
+    this.setCursor(
+        this.getToolkit().createCustomCursor(
+            new BufferedImage(
+                1,
+                1,
+                BufferedImage.TYPE_INT_ARGB),
+            new Point(0,0),
+            "blank"));
+  }
+  
+  public void setColor(Color color){
+    drawColor = color;
+  }
+  
+  public void setRadius(int radius){
+    this.radius = radius;
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      g.drawImage(canvas, 0, 0, null);
+      Point q = this.getMousePosition();
+      if(q != null)
+        g.drawOval(q.x-radius/2, q.y-radius/2, radius, radius);
+      
+  }
+  
+  public static void main(String[] args){
+    EventQueue.invokeLater(new Runnable(){
+
+      @Override
+      public void run() {
+        JFrame frame = new JFrame();
+        frame.setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.Y_AXIS));
+        
+
+        PaintPanel paint = new PaintPanel();
+        paint.setPreferredSize(new Dimension(500, 500));
+        frame.add(paint);
+        JColorChooser choose = new JColorChooser();
+        choose.setColor(Color.black);
+        choose.getSelectionModel().addChangeListener(new ChangeListener(){
+
+          @Override
+          public void stateChanged(ChangeEvent arg0) {
+            paint.setColor(choose.getColor());
+            
+          }
+          
+        });
+        frame.add(choose.getChooserPanels()[1]);
+        JSlider slide = new JSlider(1,100);
+        slide.setValue(paint.radius);
+        slide.addChangeListener(new ChangeListener(){
+
+          @Override
+          public void stateChanged(ChangeEvent e) {
+            paint.setRadius(slide.getValue());
+            
+          }
+          
+        });
+        frame.add(slide);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        paint.initialize();
+      }
+      
+    });
+  }
+
+}
